584b4768bbbb6daed92e59ef63cc1ee134e08393
Harriet Lawrence <harrietgracelawrence@gmail.com>
2017-10-13 09:24:41 +1100

    Add s3 upload

--- step-types-example/s3upload.sh
+++ step-types-example/s3upload.sh
@@ -1,1 +1,2 @@
-exit 0
+echo "Uploading log file to S3"
+buildkite-agent artifact upload step-types-example/logs.txt s3://screencast-examples/$BUILDKITE_JOB_ID
--- step-types-example/upload.sh
+++ step-types-example/upload.sh
@@ -1,3 +1,2 @@
 echo "Uploading log file as an artifact"
 buildkite-agent artifact upload step-types-example/logs.txt
-exit 0
