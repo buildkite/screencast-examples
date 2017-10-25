1f2880a4292064ae03a07d70b1d4af91c720dd7c
Harriet Lawrence <harrietgracelawrence@gmail.com>
2017-10-25 16:54:43 +1100

    Tweak step names

--- step-types-example/.buildkite/pipeline.deploy.yml.sh
+++ step-types-example/.buildkite/pipeline.deploy.yml.sh
@@ -18,14 +18,14 @@ echo 'steps:
 if [[ "${IS_ROLLBACK}" != "true" ]]; then
   echo '
   - wait
-  - block: "Rollback deploy"
+  - block: "Rollback to ${PREVIOUSLY_DEPLOYED_COMMIT}"
     prompt: "Create a rollback deployment for build ${BUILDKITE_BUILD} (to commit ${PREVIOUSLY_DEPLOYED_COMMIT})"
     fields:
       - text: "Reason"
         key: "reason"
         hint: "What is the reason for rolling this build back?"
   - trigger: "${BUILDKITE_PIPELINE_SLUG}"
-    label: "Rollback to ${PREVIOUSLY_DEPLOYED_COMMIT}"
+    label: "Trigger rollback"
     build:
       message: "Rollback"
       commit: "${PREVIOUSLY_DEPLOYED_COMMIT}"
